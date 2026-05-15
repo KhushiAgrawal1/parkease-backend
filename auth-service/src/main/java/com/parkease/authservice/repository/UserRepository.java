5661b1f26f9a5290e1ad2800520cdb71f91fa3ba
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-05-15 18:39:55 +0530

    fixed cache key issue

--- auth-service/src/main/java/com/parkease/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/parkease/authservice/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findFirstByEmail(String email);
 
-    @Cacheable(value = "users", key = "#email")
+    @Cacheable(value = "users", key = "#p0")
     User findByEmail(String email);
 
     @CachePut(value = "users", key = "#user.email")
